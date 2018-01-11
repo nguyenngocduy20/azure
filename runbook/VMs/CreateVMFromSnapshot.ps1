b5224d6b232125500160ea42e79e16071d851e20
Nguyen Ngoc Duy <nguyenngoc.duy20@gmail.com>
2018-01-11 16:29:57 +0700

    Create VM from Snapshot

---
+++ runbook/VMs/CreateVMFromSnapshot.ps1
@@ -0,0 +1,48 @@
+
+
+#Provide the name of your resource group
+$resourceGroupName ='FIS-xLAB'
+
+#Provide the name of the snapshot that will be used to create OS disk
+$snapshotName = 'Backup-1'
+
+#Provide the name of the OS disk that will be created using the snapshot
+$osDiskName = 'Ubuntu12_04_clone'
+
+#Provide the name of an existing virtual network where virtual machine will be created
+$virtualNetworkName = 'FIS-xLAB-vnet'
+
+#Provide the name of the virtual machine
+$virtualMachineName = 'Ubuntu12_04_clone'
+
+#Provide the size of the virtual machine
+#e.g. Standard_DS3
+#Get all the vm sizes in a region using below script:
+#e.g. Get-AzureRmVMSize -Location westus
+$virtualMachineSize = 'Standard_A1_v2'
+
+$snapshot = Get-AzureRmSnapshot -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName 
+ 
+$diskConfig = New-AzureRmDiskConfig -AccountType $storageType -Location $snapshot.Location -SourceResourceId $snapshot.Id -CreateOption Copy
+ 
+$disk = New-AzureRmDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $osDiskName
+
+#Initialize virtual machine configuration
+$VirtualMachine = New-AzureRmVMConfig -VMName $virtualMachineName -VMSize $virtualMachineSize
+
+#Use the Managed Disk Resource Id to attach it to the virtual machine. Please change the OS type to linux if OS disk has linux OS
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -ManagedDiskId $disk.Id -CreateOption Attach -Linux
+
+#Create a public IP for the VM  
+$publicIp = New-AzureRmPublicIpAddress -Name ($VirtualMachineName.ToLower()+'_ip') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -AllocationMethod Dynamic
+
+#Get the virtual network where virtual machine will be hosted
+$vnet = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
+
+# Create NIC in the first subnet of the virtual network 
+$nic = New-AzureRmNetworkInterface -Name ($VirtualMachineName.ToLower()+'_nic') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
+
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $nic.Id
+
+#Create the virtual machine with Managed Disk
+New-AzureRmVM -VM $VirtualMachine -ResourceGroupName $resourceGroupName -Location $snapshot.Location
